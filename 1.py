9edf5c7c6892dcd7a814d663c3cb3ea3ec28d317
jarednthomas <jaredpyrothomas@gmail.com>
2020-11-29 12:31:54 -0600

    updated solution for problem 1

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+url = "http://www.pythonchallenge.com/pc/def/map.html"
+current_page = url.removeprefix("http://www.pythonchallenge.com/pc/def/")
+current_page = current_page.removesuffix(".html")
+
+a = "abcdefghijklmnopqrstuvwzyz"
+k = "cdefghijklmnopqrstuvwzyzab"
+
+caesar_shift = current_page.maketrans(a, k)
+decoded_url = current_page.translate(caesar_shift)
+new_url = url.replace(current_page, decoded_url)
+
+print(new_url)
